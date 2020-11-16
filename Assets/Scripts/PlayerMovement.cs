a11c931fafedf4f5bd35f50bc16116256cee482b
Xing Ye <yxinghang@gmail.com>
2020-11-16 16:28:30 -0500

    added movement to gameObject prefab

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 m_Movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() 
+    { 
+        float horizontal = Input.GetAxis ("Horizontal");
+        float vertical = Input.GetAxis ("Vertical");
+        
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize ();
+
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+        bool hasverticaltalInput = !Mathf.Approximately (vertical, 0f);
+
+        bool iswalking = hasHorizontalInput || hasverticaltalInput;
+    }
+}
